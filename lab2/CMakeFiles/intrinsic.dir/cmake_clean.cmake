eab9b14f7296cf38810bf3a736bce0d0033f182b
speedyswimmer1000 <speedyswimmer1000@gmail.com>
2015-06-30 18:04:58 -0600

    small
    chunks

---
+++ lab2/CMakeFiles/intrinsic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/intrinsic.dir/lab2_5.cpp.o"
+  "intrinsic.pdb"
+  "intrinsic"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/intrinsic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
